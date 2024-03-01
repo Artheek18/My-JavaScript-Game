166d4a9c44840aae0d9d2858741918aeb398693e
Artheek18 <artheek2321.Shan@gmail.com>
2024-03-01 16:08:32 -0500

    work

---
+++ background.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+console.log(ctx);
+
+const WIDTH_CANVAS = canvas.width = 800;
+const HEIGHT_CANVAS = canvas.height = 700;
